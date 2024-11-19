f511395d9d22b928036c27441d2d905c88af4376
Reid Conner <rconner@k12k.com>
2024-11-19 17:23:59 -0500

    Update MaxPosition1IOSparkFlex.java

--- src/main/java/frc/robot/subsystems/arm1/MaxPosition1IOSparkFlex.java
+++ src/main/java/frc/robot/subsystems/arm1/MaxPosition1IOSparkFlex.java
@@ -1,13 +1,13 @@
-package frc.robot.subsystems.arm1;
+package frc.robot.subsystems.maxPosition1;
 
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkFlex;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.math.util.Units;
-import frc.robot.Constants.ArmConstants;
+import frc.robot.Constants.MaxPosition1Constants;
 
-public class Arm1IOSparkFlex implements Arm1IO {
+public class MaxPosition1IOSparkFlex implements MaxPosition1IO {
 
   private final CANSparkFlex leader = new CANSparkFlex(3, MotorType.kBrushless);
   private final RelativeEncoder motorInternalEncoder = leader.getEncoder();
@@ -18,9 +18,9 @@ public class Arm1IOSparkFlex implements Arm1IO {
   // This position is the ARM_MIN_ANGLE_DEG
   // Initialize the internal motor encoder to the shaft revolutions corresponding to this angle
   private final double intEncoderPositionStopsRev =
-      ArmConstants.ARM_MIN_ANGLE_DEG / 360.0 * ArmConstants.ARM_GEAR_REDUCTION;
+      MaxPosition1Constants.ARM_MIN_ANGLE_DEG / 360.0 * MaxPosition1Constants.ARM_GEAR_REDUCTION;
 
-  public Arm1IOSparkFlex() {
+  public MaxPosition1IOSparkFlex() {
     leader.restoreFactoryDefaults();
     leader.setCANTimeout(250);
     leader.setInverted(isMotorInverted);
@@ -42,13 +42,13 @@ public class Arm1IOSparkFlex implements Arm1IO {
   }
 
   @Override
-  public void updateInputs(Arm1IOInputs inputs) {
+  public void updateInputs(MaxPosition1IOInputs inputs) {
     inputs.internalPositionRad =
         Units.rotationsToRadians(motorInternalEncoder.getPosition())
-            / ArmConstants.ARM_GEAR_REDUCTION;
+            / MaxPosition1Constants.ARM_GEAR_REDUCTION;
     inputs.internalVelocityRadPerSec =
         Units.rotationsPerMinuteToRadiansPerSecond(motorInternalEncoder.getVelocity())
-            / ArmConstants.ARM_GEAR_REDUCTION;
+            / MaxPosition1Constants.ARM_GEAR_REDUCTION;
     inputs.appliedVolts = leader.getAppliedOutput() * leader.getBusVoltage();
     inputs.currentAmps = leader.getOutputCurrent();
     inputs.tempCelsius = leader.getMotorTemperature();
