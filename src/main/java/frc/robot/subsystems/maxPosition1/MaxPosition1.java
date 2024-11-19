ccf98074d7c3e67da6f11236316641a67e5aad1f
Mike Paulonis <web.cybertribe@gmail.com>
2024-11-19 17:25:41 -0500

    MaxPosition1.java

--- src/main/java/frc/robot/subsystems/maxPosition1/MaxPosition1.java
+++ src/main/java/frc/robot/subsystems/maxPosition1/MaxPosition1.java
@@ -1,7 +1,7 @@
-package frc.robot.subsystems.arm1;
+package frc.robot.subsystems.maxPosition11;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.MaxPosition1Feedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
@@ -15,152 +15,152 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.Constants.ArmConstants;
+import frc.robot.Constants.MaxPosition1Constants;
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.LoggedDashboardNumber;
 
-public class Arm1 extends SubsystemBase {
+public class MaxPosition1 extends SubsystemBase {
   public LoggedDashboardNumber elasticRotations =
-      new LoggedDashboardNumber("MaxPosition3ElasticRotations", 300);
-  private final Arm1IO io;
-  private final Arm1IOInputsAutoLogged inputs = new Arm1IOInputsAutoLogged();
-  private final ArmFeedforward ffModel;
+      new LoggedDashboardNumber("MaxPosition1ElasticRotations", 300);
+  private final MaxPosition1IO io;
+  private final Maxposition1IOInputsAutoLogged inputs = new MaxPosition1IOInputsAutoLogged();
+  private final MaxPosition1Feedforward ffModel;
   private final ProfiledPIDController pid;
   private Double pidOutput;
   private Double feedforwardOutput;
-  private Double angleGoalRad = Units.degreesToRadians(ArmConstants.ARM_TARGET_DEG);
-  private String armStatus = "#000000";
-  public Boolean armClosedLoop = false;
+  private Double angleGoalRad = Units.degreesToRadians(MaxPosition1Constants.MaxPosition1_TARGET_DEG);
+  private String MaxPosition1Status = "#000000";
+  public Boolean MaxPosition1ClosedLoop = false;
 
   // Create a Mechanism2d display of an Arm with a fixed ArmTower and moving Arm.
   private final Mechanism2d mech2d = new Mechanism2d(60, 60);
-  private final MechanismRoot2d armPivot = mech2d.getRoot("ArmPivot", 30, 30);
-  private final MechanismLigament2d armTower =
-      armPivot.append(new MechanismLigament2d("ArmTower", 30, -90));
-  private final MechanismLigament2d arm =
-      armPivot.append(
+  private final MechanismRoot2d MaxPosition1Pivot = mech2d.getRoot("MaxPosition1Pivot", 30, 30);
+  private final MechanismLigament2d MaxPosition1Tower =
+      MaxPosition1Pivot.append(new MechanismLigament2d("MaxPosition1Tower", 30, -90));
+  private final MechanismLigament2d MaxPosition1 =
+      MaxPosition1Pivot.append(
           new MechanismLigament2d(
-              "Arm",
+              "MaxPosition1",
               30,
               Units.radiansToDegrees(inputs.internalPositionRad),
               6,
               new Color8Bit(Color.kYellow)));
 
   /** Creates a new Arm. */
-  public Arm1(Arm1IO io) {
+  public MaxPosition11(MaxPosition11IO io) {
     this.io = io;
 
     // Put Mechanism 2d to SmartDashboard
-    SmartDashboard.putData("Arm Sim", mech2d);
-    armTower.setColor(new Color8Bit(Color.kBlue));
+    SmartDashboard.putData("MaxPosition1 Sim", mech2d);
+    MaxPosition1Tower.setColor(new Color8Bit(Color.kBlue));
 
     // Switch constants based on mode (the physics simulator is treated as a
     // separate robot with different tuning)
     switch (Constants.currentMode) {
       case REAL:
-        ffModel = new ArmFeedforward(0.0, 0.16, 6.6, 0.01);
+        ffModel = new MaxPosition1Feedforward(0.0, 0.16, 6.6, 0.01);
         pid = new ProfiledPIDController(60.0, 0.0, 1.0, new TrapezoidProfile.Constraints(1.8, 4.0));
         break;
       case REPLAY:
-        ffModel = new ArmFeedforward(0.0, 0.21, 6.93, 0.01);
+        ffModel = new MaxPosition1Feedforward(0.0, 0.21, 6.93, 0.01);
         pid = new ProfiledPIDController(80.0, 0.0, 3.0, new TrapezoidProfile.Constraints(1.7, 8.0));
         break;
       case SIM:
-        ffModel = new ArmFeedforward(0.0, 0.21, 6.93, 0.01);
+        ffModel = new MaxPosition1Feedforward(0.0, 0.21, 6.93, 0.01);
         pid = new ProfiledPIDController(80.0, 0.0, 3.0, new TrapezoidProfile.Constraints(1.7, 8.0));
         break;
       default:
-        ffModel = new ArmFeedforward(0.0, 0.0, 0.0, 0.0);
+        ffModel = new MaxPosition1Feedforward(0.0, 0.0, 0.0, 0.0);
         pid = new ProfiledPIDController(0.0, 0.0, 0.0, new TrapezoidProfile.Constraints(0.0, 0.0));
         break;
     }
-    pid.setTolerance(Units.degreesToRadians(ArmConstants.ARM_ANGLE_TOLERANCE_DEG));
+    pid.setTolerance(Units.degreesToRadians(MaxPosition1Constants.MaxPosition1_ANGLE_TOLERANCE_DEG));
   }
 
   @Override
   public void periodic() {
     io.updateInputs(inputs);
-    Logger.processInputs("Arm", inputs);
+    Logger.processInputs("MaxPosition1", inputs);
 
     double rotations = elasticRotations.get();
     setGoalDeg(rotations);
 
-    if (armClosedLoop) {
+    if (MaxPosition1ClosedLoop) {
       pidOutput = pid.calculate(inputs.internalPositionRad, angleGoalRad);
       feedforwardOutput = ffModel.calculate(pid.getSetpoint().position, pid.getSetpoint().velocity);
       io.setVoltage(pidOutput + feedforwardOutput);
 
-      Logger.recordOutput("Arm/atGoal", pid.atGoal());
-      Logger.recordOutput("Arm/angleGoalRad", angleGoalRad);
-      Logger.recordOutput("Arm/angleSPRad", pid.getSetpoint().position);
-      Logger.recordOutput("Arm/angleSPRadPerSec", pid.getSetpoint().velocity);
-      Logger.recordOutput("Arm/feedbackOP", pidOutput);
-      Logger.recordOutput("Arm/feedforwardOP", feedforwardOutput);
+      Logger.recordOutput("MaxPosition1/atGoal", pid.atGoal());
+      Logger.recordOutput("MaxPosition1/angleGoalRad", angleGoalRad);
+      Logger.recordOutput("MaxPosition1/angleSPRad", pid.getSetpoint().position);
+      Logger.recordOutput("MaxPosition1/angleSPRadPerSec", pid.getSetpoint().velocity);
+      Logger.recordOutput("MaxPosition1/feedbackOP", pidOutput);
+      Logger.recordOutput("MaxPosition1/feedforwardOP", feedforwardOutput);
 
     } else {
       pid.reset(inputs.internalPositionRad);
     }
 
     if (inputs.internalPositionRad < -0.523) {
-      armStatus = "#4CAF50"; // green
+      MaxPosition1Status = "#4CAF50"; // green
     } else if (inputs.internalPositionRad < 0.872) {
-      armStatus = "#F44336"; // red
+      MaxPosition1Status = "#F44336"; // red
     } else {
-      armStatus = "#9528CC"; // purple
+      MaxPosition1Status = "#9528CC"; // purple
     }
 
-    Logger.recordOutput("Arm/angleInternalRad", inputs.internalPositionRad);
-    Logger.recordOutput("Arm/angleInternalDeg", Units.radiansToDegrees(inputs.internalPositionRad));
-    Logger.recordOutput("Arm/motorVolts", inputs.appliedVolts);
-    Logger.recordOutput("Arm/armIsUp", armIsUp());
-    Logger.recordOutput("Arm/status", armStatus);
+    Logger.recordOutput("MaxPosition1/angleInternalRad", inputs.internalPositionRad);
+    Logger.recordOutput("MaxPosition1/angleInternalDeg", Units.radiansToDegrees(inputs.internalPositionRad));
+    Logger.recordOutput("MaxPosition1/motorVolts", inputs.appliedVolts);
+    Logger.recordOutput("MaxPosition1/MaxPosition1IsUp", MaxPosition1IsUp());
+    Logger.recordOutput("MaxPosition1/status", MaxPosition1Status);
 
     // Update the Mechanism Arm angle based on the simulated arm angle
-    arm.setAngle(Units.radiansToDegrees(inputs.internalPositionRad));
+    MaxPosition1.setAngle(Units.radiansToDegrees(inputs.internalPositionRad));
   }
 
   /** Set the goal for the arm angle. Put controller in auto if not already. */
   public void setGoalDeg(double setpointDeg) {
-    armClosedLoop = true;
+    MaxPosition1ClosedLoop = true;
     angleGoalRad =
         Units.degreesToRadians(
             MathUtil.clamp(
-                setpointDeg, ArmConstants.ARM_MIN_ANGLE_DEG, ArmConstants.ARM_MAX_ANGLE_DEG));
+                setpointDeg, MaxPosition1Constants.MaxPosition1_MIN_ANGLE_DEG, MaxPosition1Constants.MaxPosition1_MAX_ANGLE_DEG));
   }
 
   /** Stops the Arm. */
   public void stop() {
     io.stop();
-    armClosedLoop = false;
+    MaxPosition1ClosedLoop = false;
   }
 
   /** Returns the current arm angle in degrees. */
-  @AutoLogOutput(key = "Arm/angleInternalDeg")
+  @AutoLogOutput(key = "MaxPosition1/angleInternalDeg")
   public double getPositionDeg() {
     return Units.radiansToDegrees(inputs.internalPositionRad);
   }
 
   // Check if arm is within a tolerance of the down/load position.
   // Typically used as a permissive in commands.
-  public boolean armAtTarget() {
+  public boolean MaxPosition1AtTarget() {
     return Units.radiansToDegrees(inputs.internalPositionRad)
-        <= ArmConstants.ARM_TARGET_DEG + ArmConstants.ARM_IS_DOWN_TOLERANCE_DEG;
+        <= MaxPosition1Constants.MaxPosition1_TARGET_DEG + MaxPosition1Constants.MaxPosition1_IS_DOWN_TOLERANCE_DEG;
   }
 
-  public boolean armAtZero() {
+  public boolean MaxPosition1AtZero() {
     return Units.radiansToDegrees(inputs.internalPositionRad)
-        <= ArmConstants.ARM_IS_DOWN_TOLERANCE_DEG;
+        <= MaxPosition1Constants.MaxPosition1_IS_DOWN_TOLERANCE_DEG;
   }
 
   // Define "arm up" as an angle greater than 0 radians - currently no
   // shooting position would be lower than that
-  public boolean armIsUp() {
+  public boolean MaxPosition1IsUp() {
     return inputs.internalPositionRad >= 0.0;
   }
 
   // If the arm is less than 75 degrees it is not in an amp position
-  public boolean armIsNotAmped() {
+  public boolean MaxPosition1IsNotAmped() {
     return inputs.internalPositionRad < Units.degreesToRadians(75.0);
   }
 
@@ -171,11 +171,11 @@ public class Arm1 extends SubsystemBase {
   }
 
   // Move the arm to the stow/loading position.
-  public Command armToTargetCommand() {
-    return new RunCommand(() -> setGoalDeg(ArmConstants.ARM_TARGET_DEG)).until(() -> armAtTarget());
+  public Command MaxPosition1ToTargetCommand() {
+    return new RunCommand(() -> setGoalDeg(MaxPosition1Constants.MaxPosition1_TARGET_DEG)).until(() -> MaxPosition1AtTarget());
   }
 
-  public Command armToZeroCommand() {
-    return new RunCommand(() -> setGoalDeg(0.0)).until(() -> armAtZero());
+  public Command MaxPosition1ToZeroCommand() {
+    return new RunCommand(() -> setGoalDeg(0.0)).until(() -> MaxPosition1AtZero());
   }
 }
